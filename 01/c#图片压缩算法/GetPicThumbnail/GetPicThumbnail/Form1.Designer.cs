818bdbe08e75f1131d172edef231264307a1d28c
Valar Morghuils <524672142@qq.com>
2016-07-01 13:21:06 +0800

    update

---
+++ 01/c#图片压缩算法/GetPicThumbnail/GetPicThumbnail/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace GetPicThumbnail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Compression = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.ChoosePicture = new System.Windows.Forms.Button();
+            this.text_picturePath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Compression
+            // 
+            this.Compression.Location = new System.Drawing.Point(197, 216);
+            this.Compression.Name = "Compression";
+            this.Compression.Size = new System.Drawing.Size(75, 23);
+            this.Compression.TabIndex = 0;
+            this.Compression.Text = "压缩";
+            this.Compression.UseVisualStyleBackColor = true;
+            this.Compression.Click += new System.EventHandler(this.Compression_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(49, 23);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(142, 126);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // ChoosePicture
+            // 
+            this.ChoosePicture.Location = new System.Drawing.Point(197, 173);
+            this.ChoosePicture.Name = "ChoosePicture";
+            this.ChoosePicture.Size = new System.Drawing.Size(75, 23);
+            this.ChoosePicture.TabIndex = 2;
+            this.ChoosePicture.Text = "选择图片";
+            this.ChoosePicture.UseVisualStyleBackColor = true;
+            this.ChoosePicture.Click += new System.EventHandler(this.ChoosePicture_Click);
+            // 
+            // text_picturePath
+            // 
+            this.text_picturePath.Location = new System.Drawing.Point(49, 173);
+            this.text_picturePath.Name = "text_picturePath";
+            this.text_picturePath.Size = new System.Drawing.Size(142, 21);
+            this.text_picturePath.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.text_picturePath);
+            this.Controls.Add(this.ChoosePicture);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.Compression);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Compression;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button ChoosePicture;
+        private System.Windows.Forms.TextBox text_picturePath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ 01/c#图片压缩算法/GetPicThumbnail/GetPicThumbnail/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetPicThumbnail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Compression_Click(object sender, EventArgs e)
+        {
+            string SourcePath = text_picturePath.Text;
+            string TargetPath = @"D:\test.jpg";
+
+            if (!File.Exists(TargetPath))
+            {
+                File.Create(TargetPath).Close();
+            }else
+            {
+                File.Delete(TargetPath);
+                File.Create(TargetPath).Close();
+            }
+
+            GetPicThumbnail(SourcePath, TargetPath, 200, 100, 30);
+
+        }
+
+
+        public static bool GetPicThumbnail(string sFile, string dFile, int dHeight, int dWidth, int flag)
+        {
+            System.Drawing.Image iSource = System.Drawing.Image.FromFile(sFile);
+            ImageFormat tFormat = iSource.RawFormat;
+            int sW = 0, sH = 0;
+
+            //按比例缩放
+            Size tem_size = new Size(iSource.Width, iSource.Height);
+
+            if (tem_size.Width > dHeight || tem_size.Width > dWidth)
+            {
+                if ((tem_size.Width * dHeight) > (tem_size.Width * dWidth))
+                {
+                    sW = dWidth;
+                    sH = (dWidth * tem_size.Height) / tem_size.Width;
+                }
+                else
+                {
+                    sH = dHeight;
+                    sW = (tem_size.Width * dHeight) / tem_size.Height;
+                }
+            }
+            else
+            {
+                sW = tem_size.Width;
+                sH = tem_size.Height;
+            }
+
+            Bitmap ob = new Bitmap(dWidth, dHeight);
+            Graphics g = Graphics.FromImage(ob);
+
+            g.Clear(Color.WhiteSmoke);
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            g.DrawImage(iSource, new Rectangle((dWidth - sW) / 2, (dHeight - sH) / 2, sW, sH), 0, 0, iSource.Width, iSource.Height, GraphicsUnit.Pixel);
+
+            g.Dispose();
+            //以下代码为保存图片时，设置压缩质量  
+            EncoderParameters ep = new EncoderParameters();
+            long[] qy = new long[1];
+            qy[0] = flag;//设置压缩的比例1-100  
+            EncoderParameter eParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, qy);
+            ep.Param[0] = eParam;
+            try
+            {
+                ImageCodecInfo[] arrayICI = ImageCodecInfo.GetImageEncoders();
+                ImageCodecInfo jpegICIinfo = null;
+                for (int x = 0; x < arrayICI.Length; x++)
+                {
+                    if (arrayICI[x].FormatDescription.Equals("JPEG"))
+                    {
+                        jpegICIinfo = arrayICI[x];
+                        break;
+                    }
+                }
+                if (jpegICIinfo != null)
+                {
+                    ob.Save(dFile, jpegICIinfo, ep);//dFile是压缩后的新路径  
+                }
+                else
+                {
+                    ob.Save(dFile, tFormat);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                iSource.Dispose();
+                ob.Dispose();
+            }
+        }
+
+
+        private void ChoosePicture_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            if (openfile.ShowDialog() == DialogResult.OK && (openFileDialog1.FileName != ""))
+            {
+                pictureBox.ImageLocation = openfile.FileName;
+                text_picturePath.Text = openfile.FileName;
+            }
+            openfile.Dispose();
+        }
+    }
+}
