626915060d81b7315a52fee9f2d0a1d68d32d6c6
Valar Morghuils <524672142@qq.com>
2016-07-08 17:30:05 +0800

    update

---
+++ ComputerGraphics/SimpleImageProcessing/SimpleImageProcessing/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SimpleImageProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PicturePath = new System.Windows.Forms.TextBox();
+            this.Select_Picrture = new System.Windows.Forms.Button();
+            this.Run = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(76, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(326, 259);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PicturePath
+            // 
+            this.PicturePath.Location = new System.Drawing.Point(76, 306);
+            this.PicturePath.Name = "PicturePath";
+            this.PicturePath.Size = new System.Drawing.Size(277, 21);
+            this.PicturePath.TabIndex = 1;
+            // 
+            // Select_Picrture
+            // 
+            this.Select_Picrture.Location = new System.Drawing.Point(359, 304);
+            this.Select_Picrture.Name = "Select_Picrture";
+            this.Select_Picrture.Size = new System.Drawing.Size(75, 23);
+            this.Select_Picrture.TabIndex = 2;
+            this.Select_Picrture.Text = "选择图片";
+            this.Select_Picrture.UseVisualStyleBackColor = true;
+            this.Select_Picrture.Click += new System.EventHandler(this.Select_Picrture_Click);
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(359, 408);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(75, 23);
+            this.Run.TabIndex = 3;
+            this.Run.Text = "生成";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 458);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.Select_Picrture);
+            this.Controls.Add(this.PicturePath);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox PicturePath;
+        private System.Windows.Forms.Button Select_Picrture;
+        private System.Windows.Forms.Button Run;
+    }
+}
+
---
+++ ComputerGraphics/SimpleImageProcessing/SimpleImageProcessing/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimpleImageProcessing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Select_Picrture_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = openfile.FileName;
+                PicturePath.Text = openfile.FileName;
+            }
+            openfile.Dispose();
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            //以柔化效果显示图像  
+            try
+            {
+                int Height = this.pictureBox1.Image.Height;
+                int Width = this.pictureBox1.Image.Width;
+                Bitmap bitmap = new Bitmap(Width, Height);
+                Bitmap MyBitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel;
+                //高斯模板  
+                int[] Gauss = { 1, 2, 1, 2, 4, 2, 1, 2, 1 };
+                for (int x = 1; x < Width - 1; x++)
+                    for (int y = 1; y < Height - 1; y++)
+                    {
+                        int r = 0, g = 0, b = 0;
+                        int Index = 0;
+                        for (int col = -1; col <= 1; col++)
+                            for (int row = -1; row <= 1; row++)
+                            {
+                                pixel = MyBitmap.GetPixel(x + row, y + col);
+                                r += pixel.R * Gauss[Index];
+                                g += pixel.G * Gauss[Index];
+                                b += pixel.B * Gauss[Index];
+                                Index++;
+                            }
+                        r /= 16;
+                        g /= 16;
+                        b /= 16;
+                        //处理颜色值溢出  
+                        r = r > 255 ? 255 : r;
+                        r = r < 0 ? 0 : r;
+                        g = g > 255 ? 255 : g;
+                        g = g < 0 ? 0 : g;
+                        b = b > 255 ? 255 : b;
+                        b = b < 0 ? 0 : b;
+                        bitmap.SetPixel(x - 1, y - 1, Color.FromArgb(r, g, b));
+                    }
+                this.pictureBox1.Image = bitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "信息提示");
+            }
+        }
+
+
+        //以黑白效果显示图像
+        public void BlackAndWhite()
+        {
+            try
+            {
+                int Height = pictureBox1.Image.Height;
+                int Width = pictureBox1.Image.Width;
+                Bitmap newBitmap = new Bitmap(Width, Height);
+                Bitmap oldBitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel;
+                for (int x = 0; x < Width; x++)
+                {
+                    for (int y = 0; y < Height; y++)
+                    {
+                        pixel = oldBitmap.GetPixel(x, y);
+                        int r, g, b, Result = 0;
+                        r = pixel.R;
+                        g = pixel.G;
+                        b = pixel.B;
+
+                        //实例程序以加权平均值法产生黑白图像
+                        int iType = 2;
+                        switch (iType)
+                        {
+                            //平均值法
+                            case 0:
+                                Result = ((r + g + b) / 3);
+                                break;
+
+                            //最大值法
+                            case 1:
+                                Result = r > g ? r : g;
+                                Result = Result > b ? Result : b;
+                                break;
+
+                            //加权平均数
+                            case 2:
+                                Result = ((int)(0.7 * r) + (int)(0.2 * g) + (int)(0.1 * b));
+                                break;
+                        }
+
+                        newBitmap.SetPixel(x, y, Color.FromArgb(Result, Result, Result));
+                    }
+                    pictureBox1.Image = newBitmap;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "信息提示");
+            }
+        }
+
+
+
+        //以浮雕效果显示图像
+        public void relief()
+        {
+            try
+            {
+                int Height = pictureBox1.Image.Height;
+                int Width = pictureBox1.Image.Width;
+                Bitmap newBitmap = new Bitmap(Width, Height);
+                Bitmap oldBitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel1, pixel2;
+                for (int x = 0; x < Width - 1; x++)
+                {
+                    for (int y = 0; y < Height - 1; y++)
+                    {
+                        int r = 0, g = 0, b = 0;
+                        pixel1 = oldBitmap.GetPixel(x, y);
+                        pixel2 = oldBitmap.GetPixel(x + 1, y + 1);
+                        r = Math.Abs(pixel1.R - pixel2.R + 128);
+                        g = Math.Abs(pixel1.R - pixel2.R + 128);
+                        b = Math.Abs(pixel1.R - pixel2.R + 128);
+
+                        if (r > 255)
+                            r = 255;
+
+                        if (r < 0)
+                            r = 0;
+
+                        if (g > 255)
+                            g = 255;
+
+                        if (g < 0)
+                            g = 0;
+
+                        if (b > 255)
+                            b = 255;
+
+                        if (b < 0)
+                            b = 0;
+
+                        newBitmap.SetPixel(x, y, Color.FromArgb(r, g, b));
+                    }
+                }
+
+                pictureBox1.Image = newBitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "信息提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+        //以照片底片效果显示图像
+        public void Invert()
+        {
+            try
+            {
+                int Height = pictureBox1.Image.Height;
+                int Width = pictureBox1.Image.Width;
+                Bitmap newbitmap = new Bitmap(Width, Height);
+                Bitmap oldbitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel;
+                for (int x = 1; x < Width; x++)
+                {
+                    for (int y = 1; y < Height; y++)
+                    {
+                        int r, g, b;
+                        pixel = oldbitmap.GetPixel(x, y);
+                        r = 255 - pixel.R;
+                        g = 255 - pixel.G;
+                        b = 255 - pixel.B;
+                        newbitmap.SetPixel(x, y, Color.FromArgb(r, g, b));
+                    }
+                }
+                this.pictureBox1.Image = newbitmap;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "信息提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+    }
+}
