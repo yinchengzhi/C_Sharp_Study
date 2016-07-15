dcc8b60ff5e27d3457ed6c2a44e26d60b3636661
Valar Morghuils <524672142@qq.com>
2016-07-15 17:28:26 +0800

    update

---
+++ ComputerGraphics/Ellipse 3D Transform/Ellipse/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Ellipse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(12, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(565, 363);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "调整的角度:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(90, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 19);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "加10度";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(230, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 19);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "减10度";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 436);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "椭圆的轨迹";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ ComputerGraphics/Ellipse 3D Transform/Ellipse/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ellipse
+{
+    public partial class Form1 : Form
+    {
+        PointF xyPoint = new PointF();
+        PointF centerPoint = new PointF();
+        PointF movePoint = new PointF();
+        PointF xiePointF=new PointF();
+        PointF xiedPointF = new PointF();
+        private double perAngle = 0;
+        private double nowAngle;
+        private double moveAngle;
+        PointF[] pointFs=new PointF[201];
+        PointF[] pointFs1=new PointF[201];
+        PointF[] xiePointFs=new PointF[201];
+        PointF[] xiePointFs1 = new PointF[201];
+        private float a =200;
+        private float b =100;
+        private float stepLength = 2;
+        private float xLength = 0;
+        private float yLength = 0;
+        private float dbYlength = 0;
+        private float xiedLength = 0;
+        private int angle = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+                return;
+            }
+            if (this.textBox1.Text == "")
+            {
+                this.errorProvider1.SetError(this.textBox1, "请输入倾斜的角度");
+                return;
+            }
+            else
+            {
+                this.errorProvider1.SetError(this.textBox1, "");
+            }
+            xyPoint.X = this.panel1.Width/2-200;
+            xyPoint.Y = this.panel1.Height/2-100;
+            centerPoint.X = xyPoint.X + a;
+            centerPoint.Y = xyPoint.Y + b;
+            Graphics graphics = this.panel1.CreateGraphics();
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.Clear(this.panel1.BackColor);
+            Pen pen = new Pen(Color.Red);
+            for (int index = 0; index < pointFs.Length; index++)
+            {
+                movePoint.X = index * stepLength + xyPoint.X;
+                xLength = centerPoint.X - movePoint.X;
+                yLength = (float)(Math.Sqrt((1 - (xLength * xLength) / 40000) * 10000));
+                movePoint.Y = b - yLength + xyPoint.Y;
+                pointFs[index] = movePoint;
+            }
+            graphics.DrawCurve(pen, pointFs, 1.5f);
+            for (int index = 0; index < pointFs1.Length; index++)
+            {
+                dbYlength = centerPoint.Y - pointFs[index].Y;
+                pointFs1[index].Y = centerPoint.Y + dbYlength;
+                pointFs1[index].X = pointFs[index].X;
+            }
+            graphics.DrawCurve(pen, pointFs1, 1.5f);
+            for (int index = 0; index < pointFs.Length; index++)
+            {
+                xiePointF = pointFs[index];
+                double startAngle = Math.Atan2(xiePointF.Y - centerPoint.Y, xiePointF.X - centerPoint.X);
+                string jiaodu = this.textBox1.Text;
+                perAngle = int.Parse(jiaodu) * Math.PI / 180;
+                nowAngle = startAngle + perAngle;
+                xiedLength = (float)Math.Sqrt(Math.Pow(xiePointF.X - centerPoint.X, 2) + Math.Pow(xiePointF.Y - centerPoint.Y, 2));
+                xiedPointF.X = centerPoint.X - (float)(xiedLength * Math.Cos(nowAngle));
+                xiedPointF.Y = centerPoint.Y + (float)(xiedLength * Math.Sin(nowAngle));
+                xiePointFs[index] = xiedPointF;
+            }
+            graphics.DrawCurve(pen, xiePointFs, 1.5f);
+            for (int index = 0; index < pointFs1.Length; index++)
+            {
+                xiePointF = pointFs1[index];
+                double startAngle = Math.Atan2(xiePointF.Y - centerPoint.Y, xiePointF.X - centerPoint.X);
+                string jiaodu = this.textBox1.Text;
+                perAngle = int.Parse(jiaodu) * Math.PI / 180;
+                nowAngle = startAngle + perAngle;
+                xiedLength = (float)Math.Sqrt(Math.Pow(xiePointF.X - centerPoint.X, 2) + Math.Pow(xiePointF.Y - centerPoint.Y, 2));
+                xiedPointF.X = centerPoint.X - (float)(xiedLength * Math.Cos(nowAngle));
+                xiedPointF.Y = centerPoint.Y + (float)(xiedLength * Math.Sin(nowAngle));
+                xiePointFs1[index] = xiedPointF;
+            }
+            graphics.DrawCurve(pen, xiePointFs1, 1.5f);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            angle += 10;
+            this.textBox1.Text = angle.ToString();
+            if (this.textBox1.Text == "")
+            {
+                this.errorProvider1.SetError(this.textBox1, "请输入倾斜的角度");
+                return;
+            }
+            else
+            {
+                this.errorProvider1.SetError(this.textBox1, "");
+            }
+            xyPoint.X = this.panel1.Width / 2-200;
+            xyPoint.Y = this.panel1.Height / 2-100;
+            centerPoint.X = xyPoint.X + a;
+            centerPoint.Y = xyPoint.Y + b;
+            Graphics graphics = this.panel1.CreateGraphics();
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.Clear(this.panel1.BackColor);
+            Pen pen = new Pen(Color.Red);
+            for (int index = 0; index < pointFs.Length; index++)
+            {
+                movePoint.X = index * stepLength + xyPoint.X;
+                xLength = centerPoint.X - movePoint.X;
+                yLength = (float)(Math.Sqrt((1 - (xLength * xLength) / 40000) * 10000));
+                movePoint.Y = b - yLength + xyPoint.Y;
+                pointFs[index] = movePoint;
+            }
+            graphics.DrawCurve(pen, pointFs, 1.5f);
+            for (int index = 0; index < pointFs1.Length; index++)
+            {
+                dbYlength = centerPoint.Y - pointFs[index].Y;
+                pointFs1[index].Y = centerPoint.Y + dbYlength;
+                pointFs1[index].X = pointFs[index].X;
+            }
+            graphics.DrawCurve(pen, pointFs1, 1.5f);
+            for (int index = 0; index < pointFs.Length; index++)
+            {
+                xiePointF = pointFs[index];
+                double startAngle = Math.Atan2(xiePointF.Y - centerPoint.Y, xiePointF.X - centerPoint.X);
+                string jiaodu = this.textBox1.Text;
+                perAngle = int.Parse(jiaodu) * Math.PI / 180;
+                nowAngle = startAngle + perAngle;
+                xiedLength = (float)Math.Sqrt(Math.Pow(xiePointF.X - centerPoint.X, 2) + Math.Pow(xiePointF.Y - centerPoint.Y, 2));
+                xiedPointF.X = centerPoint.X - (float)(xiedLength * Math.Cos(nowAngle));
+                xiedPointF.Y = centerPoint.Y + (float)(xiedLength * Math.Sin(nowAngle));
+                xiePointFs[index] = xiedPointF;
+            }
+            graphics.DrawCurve(pen, xiePointFs, 1.5f);
+            for (int index = 0; index < pointFs1.Length; index++)
+            {
+                xiePointF = pointFs1[index];
+                double startAngle = Math.Atan2(xiePointF.Y - centerPoint.Y, xiePointF.X - centerPoint.X);
+                string jiaodu = this.textBox1.Text;
+                perAngle = int.Parse(jiaodu) * Math.PI / 180;
+                nowAngle = startAngle + perAngle;
+                xiedLength = (float)Math.Sqrt(Math.Pow(xiePointF.X - centerPoint.X, 2) + Math.Pow(xiePointF.Y - centerPoint.Y, 2));
+                xiedPointF.X = centerPoint.X - (float)(xiedLength * Math.Cos(nowAngle));
+                xiedPointF.Y = centerPoint.Y + (float)(xiedLength * Math.Sin(nowAngle));
+                xiePointFs1[index] = xiedPointF;
+            }
+            graphics.DrawCurve(pen, xiePointFs1, 1.5f);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            angle -= 10;
+            this.textBox1.Text = angle.ToString();
+            if (this.textBox1.Text == "")
+            {
+                this.errorProvider1.SetError(this.textBox1, "请输入倾斜的角度");
+                return;
+            }
+            else
+            {
+                this.errorProvider1.SetError(this.textBox1, "");
+            }
+            xyPoint.X = this.panel1.Width / 2-200;
+            xyPoint.Y = this.panel1.Height / 2-100;
+            centerPoint.X = xyPoint.X + a;
+            centerPoint.Y = xyPoint.Y + b;
+            Graphics graphics = this.panel1.CreateGraphics();
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.Clear(this.panel1.BackColor);
+            Pen pen = new Pen(Color.Red);
+            for (int index = 0; index < pointFs.Length; index++)
+            {
+                movePoint.X = index * stepLength + xyPoint.X;
+                xLength = centerPoint.X - movePoint.X;
+                yLength = (float)(Math.Sqrt((1 - (xLength * xLength) / 40000) * 10000));
+                movePoint.Y = b - yLength + xyPoint.Y;
+                pointFs[index] = movePoint;
+            }
+            graphics.DrawCurve(pen, pointFs, 1.5f);
+            for (int index = 0; index < pointFs1.Length; index++)
+            {
+                dbYlength = centerPoint.Y - pointFs[index].Y;
+                pointFs1[index].Y = centerPoint.Y + dbYlength;
+                pointFs1[index].X = pointFs[index].X;
+            }
+            graphics.DrawCurve(pen, pointFs1, 1.5f);
+            for (int index = 0; index < pointFs.Length; index++)
+            {
+                xiePointF = pointFs[index];
+                double startAngle = Math.Atan2(xiePointF.Y - centerPoint.Y, xiePointF.X - centerPoint.X);
+                string jiaodu = this.textBox1.Text;
+                perAngle = int.Parse(jiaodu) * Math.PI / 180;
+                nowAngle = startAngle + perAngle;
+                xiedLength = (float)Math.Sqrt(Math.Pow(xiePointF.X - centerPoint.X, 2) + Math.Pow(xiePointF.Y - centerPoint.Y, 2));
+                xiedPointF.X = centerPoint.X - (float)(xiedLength * Math.Cos(nowAngle));
+                xiedPointF.Y = centerPoint.Y + (float)(xiedLength * Math.Sin(nowAngle));
+                xiePointFs[index] = xiedPointF;
+            }
+            graphics.DrawCurve(pen, xiePointFs, 1.5f);
+            for (int index = 0; index < pointFs1.Length; index++)
+            {
+                xiePointF = pointFs1[index];
+                double startAngle = Math.Atan2(xiePointF.Y - centerPoint.Y, xiePointF.X - centerPoint.X);
+                string jiaodu = this.textBox1.Text;
+                perAngle = int.Parse(jiaodu) * Math.PI / 180;
+                nowAngle = startAngle + perAngle;
+                xiedLength = (float)Math.Sqrt(Math.Pow(xiePointF.X - centerPoint.X, 2) + Math.Pow(xiePointF.Y - centerPoint.Y, 2));
+                xiedPointF.X = centerPoint.X - (float)(xiedLength * Math.Cos(nowAngle));
+                xiedPointF.Y = centerPoint.Y + (float)(xiedLength * Math.Sin(nowAngle));
+                xiePointFs1[index] = xiedPointF;
+            }
+            graphics.DrawCurve(pen, xiePointFs1, 1.5f);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            angle = int.Parse(this.textBox1.Text);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+           Point ePoint=new Point();
+            ePoint.X = e.X;
+            ePoint.Y = e.Y;
+            moveAngle = Math.PI/2-Math.Atan2(ePoint.Y - centerPoint.Y, ePoint.X - centerPoint.X) ;
+            xyPoint.X = this.panel1.Width / 2 - 200;
+            xyPoint.Y = this.panel1.Height / 2 - 100;
+            centerPoint.X = xyPoint.X + a;
+            centerPoint.Y = xyPoint.Y + b;
+            Graphics graphics = this.panel1.CreateGraphics();
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.Clear(this.panel1.BackColor);
+            Pen pen = new Pen(Color.Red);
+            for (int index = 0; index < pointFs.Length; index++)
+            {
+                movePoint.X = index * stepLength + xyPoint.X;
+                xLength = centerPoint.X - movePoint.X;
+                yLength = (float)(Math.Sqrt((1 - (xLength * xLength) / 40000) * 10000));
+                movePoint.Y = b - yLength + xyPoint.Y;
+                pointFs[index] = movePoint;
+            }
+            graphics.DrawCurve(pen, pointFs, 1.5f);
+            for (int index = 0; index < pointFs1.Length; index++)
+            {
+                dbYlength = centerPoint.Y - pointFs[index].Y;
+                pointFs1[index].Y = centerPoint.Y + dbYlength;
+                pointFs1[index].X = pointFs[index].X;
+            }
+            graphics.DrawCurve(pen, pointFs1, 1.5f);
+            for (int index = 0; index < pointFs.Length; index++)
+            {
+                xiePointF = pointFs[index];
+                double startAngle = Math.Atan2(xiePointF.Y - centerPoint.Y, xiePointF.X - centerPoint.X);
+                string jiaodu = this.textBox1.Text;
+                //perAngle = int.Parse(jiaodu) * Math.PI / 180;
+                perAngle = moveAngle;
+                nowAngle = startAngle + perAngle;
+                xiedLength = (float)Math.Sqrt(Math.Pow(xiePointF.X - centerPoint.X, 2) + Math.Pow(xiePointF.Y - centerPoint.Y, 2));
+                xiedPointF.X = centerPoint.X - (float)(xiedLength * Math.Cos(nowAngle));
+                xiedPointF.Y = centerPoint.Y + (float)(xiedLength * Math.Sin(nowAngle));
+                xiePointFs[index] = xiedPointF;
+            }
+            graphics.DrawCurve(pen, xiePointFs, 1.5f);
+            for (int index = 0; index < pointFs1.Length; index++)
+            {
+                xiePointF = pointFs1[index];
+                double startAngle = Math.Atan2(xiePointF.Y - centerPoint.Y, xiePointF.X - centerPoint.X);
+                string jiaodu = this.textBox1.Text;
+                //perAngle = int.Parse(jiaodu) * Math.PI / 180;
+                perAngle = moveAngle;
+                nowAngle = startAngle + perAngle;
+                xiedLength = (float)Math.Sqrt(Math.Pow(xiePointF.X - centerPoint.X, 2) + Math.Pow(xiePointF.Y - centerPoint.Y, 2));
+                xiedPointF.X = centerPoint.X - (float)(xiedLength * Math.Cos(nowAngle));
+                xiedPointF.Y = centerPoint.Y + (float)(xiedLength * Math.Sin(nowAngle));
+                xiePointFs1[index] = xiedPointF;
+            }
+            graphics.DrawCurve(pen, xiePointFs1, 1.5f);
+        }
+
+        
+    }
+}
